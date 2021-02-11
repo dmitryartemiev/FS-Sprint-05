cf93d7b1084d004792f0248150bb6304bb1f51f7
dmitryartemiev <dima77261@gmail.com>
2021-02-11 14:38:38 +0200

    t05 is done (get set)

---
+++ t05/hard-worker.js
@@ -0,0 +1,36 @@
+"use strict";
+
+module.exports = class HardWorker {
+  constructor(name, age, salary) {
+    this.name = name;
+    this.age = age;
+    this.salary = salary;
+  }
+  get age(){
+      return this._age
+  }
+  set age(value) {
+    if (value <= 1 || value > 100) {
+      return;
+    } else {
+      this._age = value;
+    }
+  }
+  get salary(){
+    return this._salary
+  }
+  set salary(value) {
+    if (value <= 100 || value > 10000) {
+      return;
+    } else {
+      this._salary = value;
+    }
+  }
+  toObject(){
+    return {
+        name: this.name,
+        age : this.age,
+        salary : this.salary
+    }
+  }
+};
